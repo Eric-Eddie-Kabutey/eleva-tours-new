b5416859469bbba40451be612369a064418de27c
touraye <touraye07@gmail.com>
2025-11-15 12:20:58 +0000

    add module - hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { Hero } from "@/components/home/hero/hero";
+
 export default function Home() {
     return (<>
         {/* Hero section */}
-        <h1>Hello, from Eleva Tours.</h1>
+        <Hero />
     </>)
 }
