887d311d2e2de3f5628cde9d61bf19983ff6f4ae
touraye <touraye07@gmail.com>
2025-11-15 16:27:22 +0000

    add new hero bg-images

--- lib/hero-slides-data.ts
+++ lib/hero-slides-data.ts
@@ -5,7 +5,7 @@ export const heroSlides = [
         titleLine1: "Start Your Travel",
         titleLine2: "With Eleva Tours",
         description: "Fine Hotel And Ticket With Best Experience. Intrinsically architect superior core competencies vis-a-vis interactive partnerships.",
-        imageUrl: "/assets/images/home/hero-image-1.avif", 
+        imageUrl: "/assets/images/home/hero-bg-1.jpg", 
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ export const heroSlides = [
         titleLine1: "Experience Memorable",
         titleLine2: "Moments",
         description: "Fine Hotel And Ticket With Best Experience. Intrinsically architect superior core competencies vis-a-vis interactive partnerships.",
-        imageUrl: "/assets/images/home/hero-image-2.avif",
+        imageUrl: "/assets/images/home/hero-bg-2.jpg",
     },
     {
         id: 3,
@@ -21,6 +21,6 @@ export const heroSlides = [
         titleLine1: "Enjoy Holiday By",
         titleLine2: "Traveling",
         description: "Fine Hotel And Ticket With Best Experience. Intrinsically architect superior core competencies vis-a-vis interactive partnerships.",
-        imageUrl: "/assets/images/home/hero-image-3.avif",
+        imageUrl: "/assets/images/home/hero-bg-3.jpg",
     },
 ];
