380e8b9fbcc8d3cedff52f1ad9b29c9fd601d25e
touraye <touraye07@gmail.com>
2025-11-15 09:58:51 +0000

    add data -navigation

---
+++ lib/nav-data.ts
@@ -0,0 +1,34 @@
+export const navLinks = [
+    { title: "Home", href: "/" },
+    { title: "About Us", href: "/about-us" },
+    {
+        title: "Destinations",
+        links: [
+            { title: "Ghana", href: "/destinations/ghana" },
+            { title: "South Africa", href: "/destinations/south-africa" },
+        ]
+    },
+    {
+        title: "Trip Types",
+        links: [
+            { title: "Family Holiday", href: "/tours/family" },
+            { title: "Group Tours", href: "/tours/group" },
+            { title: "Solo Trip", href: "/tours/solo" },
+        ]
+    },    
+    {
+        title: "Insights", links: [
+            { title: "News & Stories", href: "/news" },
+            { title: "Client Feedbacks", href: "/feedbacks" },
+            { title: "Media", href: "/tours-media" },
+     ] },
+    { title: "FAQ", href: "/help" },
+];
+
+export const socialLinks = [
+    { name: 'Facebook', href: '#', icon: 'Facebook' },
+    { name: 'Twitter', href: '#', icon: 'Twitter' },
+    { name: 'LinkedIn', href: '#', icon: 'Linkedin' },
+    { name: 'Instagram', href: '#', icon: 'Instagram' },
+    { name: 'Youtube', href: '#', icon: 'Youtube' },
+];
