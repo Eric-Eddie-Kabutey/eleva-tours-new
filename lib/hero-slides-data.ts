b2c2548232a70fc905ec7c7482689e34c73a80d0
touraye <touraye07@gmail.com>
2025-11-15 12:18:03 +0000

    add data - hero slides

---
+++ lib/hero-slides-data.ts
@@ -0,0 +1,26 @@
+export const heroSlides = [
+    {
+        id: 1,
+        preTitle: "Relax & Enjoy",
+        titleLine1: "Start Your Travel",
+        titleLine2: "With Eleva Tours",
+        description: "Fine Hotel And Ticket With Best Experience. Intrinsically architect superior core competencies vis-a-vis interactive partnerships.",
+        imageUrl: "/assets/images/home/hero-image-1.avif", 
+    },
+    {
+        id: 2,
+        preTitle: "Unforgettable Journeys",
+        titleLine1: "Experience Memorable",
+        titleLine2: "Moments",
+        description: "Fine Hotel And Ticket With Best Experience. Intrinsically architect superior core competencies vis-a-vis interactive partnerships.",
+        imageUrl: "/assets/images/home/hero-image-2.avif",
+    },
+    {
+        id: 3,
+        preTitle: "Sun, Sand & Sea",
+        titleLine1: "Enjoy Holiday By",
+        titleLine2: "Traveling",
+        description: "Fine Hotel And Ticket With Best Experience. Intrinsically architect superior core competencies vis-a-vis interactive partnerships.",
+        imageUrl: "/assets/images/home/hero-image-3.avif",
+    },
+];
