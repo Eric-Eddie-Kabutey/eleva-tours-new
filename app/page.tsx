effc693fdf5f1dd64597034671268a4d4fa180a8
touraye <touraye07@gmail.com>
2025-11-15 18:38:55 +0000

    add modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
 import { Hero } from "@/components/home/hero/hero";
+import { About } from "@/components/home/about/about";
+import { Stats } from "@/components/home/stats";
+import { PopularDestinations } from "@/components/home/popular-destination";
+import { WhyChooseUs } from "@/components/home/why-chose-us";
+import { PopularTours } from "@/components/home/popular-tours";
 
 export default function Home() {
     return (<>
         {/* Hero section */}
         <Hero />
+
+        {/* About section */}
+        <About />
+
+        {/* Statistics section */}
+        <Stats />
+
+        {/* Popular Destination section */}
+        <PopularDestinations />
+
+        {/* Why Choose Us section */}
+        <WhyChooseUs />
+
+        {/* Popular Tours section */}
+        <PopularTours />
     </>)
 }
