fc361fb8dbf797479e2f673ec3a5013c40a1c7c2
touraye <touraye07@gmail.com>
2025-11-17 13:22:21 +0000

    add data - footer

---
+++ lib/footer-data.ts
@@ -0,0 +1,21 @@
+export const footerQuickLinks = [
+    { title: "About Us", href: "/about" },
+    { title: "Tour", href: "/tours" },
+    { title: "Destinations", href: "/destinations" },
+    { title: "Blog", href: "/blog" },
+    { title: "Contact Us", href: "/contact" },
+];
+
+export const footerSocialLinks = [
+    { name: "Facebook", href: "#" },    
+    { name: "Instagram", href: "#" },    
+];
+
+export const footerGalleryImages = [
+    "/gallery/thumb-1.jpg", // Replace with your image paths
+    "/gallery/thumb-2.jpg",
+    "/gallery/thumb-3.jpg",
+    "/gallery/thumb-4.jpg",
+    "/gallery/thumb-5.jpg",
+    "/gallery/thumb-6.jpg",
+];
