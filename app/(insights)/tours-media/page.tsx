4d8e03a98146eacd849c633d0b0c588806ef7e2b
touraye <touraye07@gmail.com>
2025-11-27 14:58:17 +0000

    add tour-media page

--- app/(insights)/tours-media/page.tsx
+++ app/(insights)/tours-media/page.tsx
@@ -1,12 +1,19 @@
 import { PageHero } from "@/components/shared/page-hero";
+import { ImageGallery } from "@/components/gallery/image-gallery";
+import { VideoGallery } from "@/components/gallery/video-gallery";
 
-export default function TourMediaPage() {
-    return (<>
-        {/* Hero */}
-        <PageHero
-            title="Tours Media"
-            breadcrumbs={[{ label: "Home", href: "/" }, { label: "Tours Media", href: "/news" }]}
-            imageUrl="/assets/images/page-banner-img.jpg"
-        />
-    </>)
+export default function GalleryPage() {
+    return (
+        <main>
+            <PageHero
+                title="Tours Media"
+                breadcrumbs={[ { label: "Home", href: "/" }, { label: "Tours Media", href: "/news" } ]}
+                imageUrl="/assets/images/page-banner-img.jpg"
+            />
+
+            {/* Render both gallery components */}
+            <ImageGallery />
+            <VideoGallery />
+        </main>
+    );
 }
